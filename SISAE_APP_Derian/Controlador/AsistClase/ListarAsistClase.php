81f98d9752f252d5f97d7797e494d2810f342353
Anyulieth <anyuvillalobos@gmail.com>
2017-09-29 12:04:17 -0600

    Add files via upload

---
+++ SISAE_APP_Derian/Controlador/AsistClase/ListarAsistClase.php
@@ -0,0 +1,3 @@
+<?php include "../Controlador/conexion.php";
+$result = $con->query("CALL PaAsistClaseTb18_Listar()");
+?>
---
+++ SISAE_APP_Derian/Controlador/Funcionario/ListarFunc.php
@@ -0,0 +1,3 @@
+<?php include "../Controlador/conexion.php";
+$result = $con->query("CALL PaFunTb06_Listar()");
+?>
